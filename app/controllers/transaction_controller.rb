cd1e7d68399b3199a9ca2b8df6da1c5c319d3f3c
Randall-Mcgoy <randall_abi@outlook.com>
2018-12-08 14:14:32 -0700

    adding final comments / notes to reviewers

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,3 +1,6 @@
+#Hello this is Randall.  Bear in mind I knew nothing about Ethereum before making this project.
+  #...but I learned about Ethereum as I as I went and I really enjoyed it.  Thanks for the project.
+
 class TransactionController < ApplicationController
 require 'awesome_print'
 require 'json'
