4f95e9bffb762a2824024e81184db407bb52786f
Randall-Mcgoy <randall_abi@outlook.com>
2018-12-12 11:01:58 -0700

    more transaction details added the index page.  increased modularity of models.

--- app/helpers/transaction_helper.rb
+++ app/helpers/transaction_helper.rb
@@ -2,26 +2,38 @@ module TransactionHelper
   def api_ethscan
     #API call uses eth_getTransactionByHash to get basic informaiton, then calls tx.full_data() to obtain data
     #...by address.
-    @txInfo['response'] = JSON.parse HTTParty.get("#{@url}").response.body
+    txInfo = {}
+    full_response = JSON.parse HTTParty.get("#{@url}").response.body
+    txInfo['response'] = full_response['result']
+
     #set mode
-    if(@txInfo['response']['result'])
-      @txInfo['response']['result']['type'] = params[:mode]
+    if(txInfo['response'])
+      txInfo['response']['type'] = params[:mode]
     end
-    #save new transaction if valid response
-    if(@txInfo['response']['result'])
+    txInfo['response'] = JSON.parse make_transaction(txInfo).data
+    return txInfo['response']
+  end #end api_ethscan()
+
+  def make_transaction(txInfo)
+    #save new transaction if valid response.  returns saved transaction
+    if(txInfo['response'])
       tx = Transaction.new
-      tx.Thash = @txInfo['hash']
-      tx.data= @txInfo['response'].to_json
-      tx.api_ethaddr(params[:mode])
+      tx.Thash = params[:hash].strip
+      tx.data= txInfo['response'].to_json
+      tx.api_ethaddr(params[:mode])  #expand data with API call (by address)
       tx.pretty_values()
 
-      @txInfo['response'] = JSON.parse tx.data
       if (tx.save)
         puts "transaction saved"
       else
         puts "transaction not saved"
       end
-    end
-  end #end api_ethscan()
 
+      return tx
+    else
+      return nil
+    end
+
+
+  end #end make_transaction
 end #end module
