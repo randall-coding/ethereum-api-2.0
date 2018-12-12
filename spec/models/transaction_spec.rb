8aa9b7a0f36fe4bb2b0ea198d82136247ccd8ced
Randall-Mcgoy <randall_abi@outlook.com>
2018-12-12 08:56:50 -0700

    tests updated

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-    tx_good = Transaction.new(txHash:   , data:)
+    tx_good = Transaction.new(txHash: "0x3419e1374dcb8ff1722652…edefeb0239e2393c557b89f"   , data: )
     tx_bad = Transaction.new(txHash:nil,data:nil)
 end
