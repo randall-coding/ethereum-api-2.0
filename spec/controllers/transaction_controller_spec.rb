d25908cbcf6ddc83c3e38be078b6616e586045e6
Randall-Coding <39175191+Randall-Coding@users.noreply.github.com>
2018-12-28 16:15:14 -0700

    Updating development with master (#7)
    * Hot fix: improving tests and refactoring (#6)
    
    * Hot fix: readme (#5)

--- spec/controllers/transaction_controller_spec.rb
+++ spec/controllers/transaction_controller_spec.rb
@@ -1,20 +1,40 @@
 require 'rails_helper'
+require 'factories/transaction_factory'
 
-RSpec.describe TransactionController, type: :controller do
+RSpec.describe TransactionController, type: :controller, :aggregate_failures => true do
+  before(:all) do
+    clearDB()
+  end
+  after(:all) do
+    clearDB()
+  end
 
    describe "caching" do
-  #   describe "stores data if not already stored" do
-  #     it "has nil @cached variable" do
-  #       get :index
-  #       expect(@cached).to be_nil
-  #     end
-  #   end
-    describe "uses @cached if already in database" do
-      fixtures :transactions
-      it "should be a Transaction object" do
+    context "uses cache object if already in database, it" do
+      before(:all) do
+        cached_transaction = makeTransaction2()
+        cached_transaction.save
+      end
+      it "should be a transaction object" do
+        post :index, :params =>{:hash=>"0x79f333748c61a896d21c6f4d8dd9c8431107be0ba12b440a92fa4da058b3810d",:mode=>"mainnet"}
+        expect(@controller.view_assigns['cached'].class).to eq(Transaction)
+      end
+      it "should be the right one" do
         post :index, :params =>{:hash=>"0x79f333748c61a896d21c6f4d8dd9c8431107be0ba12b440a92fa4da058b3810d",:mode=>"mainnet"}
         expect(@controller.view_assigns['cached'].txHash).to eq("0x79f333748c61a896d21c6f4d8dd9c8431107be0ba12b440a92fa4da058b3810d")
-        end
-    end
+      end
+    end #happy path
+
+    context "does not use cache object if not found in database, it" do
+       it "should be nil (Mainnet)" do
+         post :index, :params=> {:hash => "ShouldNotBeFound", :mode => 'mainnet'}
+         expect(@controller.view_assigns['cached']).to be_falsey
+       end
+       it "should be nil (Ropsten)" do
+         post :index, :params=> {:hash => "ShouldNotBeFound", :mode => 'ropsten'}
+         expect(@controller.view_assigns['cached']).to be_falsey
+       end
+    end #mistake path
+
   end #end caching
 end #end TransactionController
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,15 +1,26 @@
 require 'rails_helper'
+require 'factories/transaction_factory'
 
-RSpec.describe Transaction, type: :model do
-  tx_good = Transaction.new(txHash: "0x4f85d4e93eaa5cd77fd79671b175b380cfc1f8d9635990c14bddb8725efa5250"   , data: '{"blockHash":"0xdc574dbf360028974fb137334a9452e2d83c3008bb5f03c0fff52c4382c1af43","blockNumber":"0x263a7d","from":"0x7fdf06809fb9069226ef1d291e4d0ba9b4b717f6","gas":"0xe57e0","gasPrice":"0.00000004","hash":"0x3419e1374dcb8ff1722652d1eab79bfdcf9348c25edefeb0239e2393c557b89f","input":"0xe75623d8000000000000000000000000940702aa202322d67214872d272ae61f1bb8eebf","nonce":"66","to":"0xf722717d938bc14cd66fe558135d665d90c955b8","transactionIndex":"0x16","value":"0.0","v":"0x1b","r":"0x291c4c510365aa7bcc695e0cb10acda0c47713b759a7df8bfc8be0cde8cf6c5b","s":"0x6b795bf87d2bc53fd44e47e14ae02f5f15baae5e8121ba95a90ae15da9705ca0","type":"mainnet","txreceipt_status":"","timeStamp":"1477400858","gasUsed":"31725","cost":null,"position":"22"}')
+RSpec.describe Transaction, type: :model , :aggregate_failures => true do
+  tx_good = makeTransaction1()
+
+  before(:all) do
+    clearDB()
+  end
+  after(:all) do
+    clearDB()
+  end
 
   it "saves good transaction" do
     expect(tx_good.save).to be true
   end
 
   describe "doesn't save bad transactions," do
-    fixtures :transactions
+    before(:all){
+      tx_good.save()
+    }
     tx_bad_nil = Transaction.new(txHash:nil,data:nil)
+
     it "doesn't save nil values" do
       expect(tx_bad_nil.save).to be_falsey
 
@@ -21,16 +32,12 @@ RSpec.describe Transaction, type: :model do
       expect(tx_bad_nil.save).to be_falsey
     end
     it "doesn't save duplicate hash values" do
-      tx_bad_dup = Transaction.new(txHash:transactions(:one).txHash, data: transactions(:one).data)
+      tx_bad_dup = makeTransaction1()
       expect(tx_bad_dup.save).to be_falsey
     end
   end
 
-  describe "fixture" do
-    fixtures :transactions
-    it "provides the right data" do
-      expect(transactions(:one).txHash).to eq('0x6f61ee4dc528443d8b6ab12240a53588fb72c659e1abfe6e6cc0d2346359be4a')
-      expect(transactions(:one).data).to eq('{"blockHash":"0xdc574dbf360028974fb137334a9452e2d83c3008bb5f03c0fff52c4382c1af43","blockNumber":"0x263a7d","from":"0x7fdf06809fb9069226ef1d291e4d0ba9b4b717f6","gas":"0xe57e0","gasPrice":"0.00000004","hash":"0x3419e1374dcb8ff1722652d1eab79bfdcf9348c25edefeb0239e2393c557b89f","input":"0xe75623d8000000000000000000000000940702aa202322d67214872d272ae61f1bb8eebf","nonce":"66","to":"0xf722717d938bc14cd66fe558135d665d90c955b8","transactionIndex":"0x16","value":"0.0","v":"0x1b","r":"0x291c4c510365aa7bcc695e0cb10acda0c47713b759a7df8bfc8be0cde8cf6c5b","s":"0x6b795bf87d2bc53fd44e47e14ae02f5f15baae5e8121ba95a90ae15da9705ca0","type":"mainnet","txreceipt_status":"","timeStamp":"1477400858","gasUsed":"31725","cost":null,"position":"22"}')
-    end
+  describe "makes API calls", :aggregate_failures => true do
+    pending "need to finish this one"
   end
-end
+end #end transaction spec
